50c1c424791cf4b22763e753dcdfa80b010e90a2
simone-poli <polismn97@gmail.com>
2025-08-06 14:42:29 +0200

    add loader in homepage with ldrs pack

---
+++ component/Loader.jsx
@@ -0,0 +1,13 @@
+export default function Loader(){
+
+    return (
+
+         <div className="text-center" role="status">
+                <l-bouncy
+                    size="45"
+                    speed="1.75"
+                    color="black"
+                ></l-bouncy>
+            </div>
+    )
+}
--- src/pages/SingleMovie.jsx
+++ src/pages/SingleMovie.jsx
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import { useNavigate } from "react-router-dom"
+import { bouncy } from 'ldrs'
+import Loader from "../../component/Loader"
+bouncy.register()
+
+
 
 
 export default function SingleMovie() {
@@ -13,42 +18,43 @@ export default function SingleMovie() {
     })
     const api_url = `http://localhost:3030/api/movies/${id}`
     const [singleMovie, setSingleMovie] = useState([])
+    const [loading, setLoading] = useState(true)
     const navigate = useNavigate()
 
 
 
-    function handleSubmit(e){
+    function handleSubmit(e) {
         e.preventDefault()
 
-        fetch(`${api_url}/reviews` , {
+        fetch(`${api_url}/reviews`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(formData)
         })
-        .then(res => res.json())
-        .then(data => {
-            console.log(data)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
 
 
-            if(data.error) {
-                console.error(data.message)
-                return
-            }
+                if (data.error) {
+                    console.error(data.message)
+                    return
+                }
 
 
-            setSingleMovie(prevState => ({
-                ...prevState,
-                reviews: [...prevState.reviews, data.review]
-            }))
+                setSingleMovie(prevState => ({
+                    ...prevState,
+                    reviews: [...prevState.reviews, data.review]
+                }))
 
-            setFormData({
-                name: '',
-                vote: 1,
-                text: ''
+                setFormData({
+                    name: '',
+                    vote: 1,
+                    text: ''
+                })
             })
-        })
     }
 
 
@@ -58,16 +64,22 @@ export default function SingleMovie() {
             .then(res => res.json())
             .then(data => {
                 console.log(data)
-                
-                if(data.error){
-                    if(data.message === 'Not found'){
+
+                if (data.error) {
+                    if (data.message === 'Not found') {
                         return navigate('/not-found')
                     }
                 }
                 setSingleMovie(data)
+                setLoading(false)
             })
     }, [])
 
+    if (loading) {
+        return (
+           <Loader />
+        )
+    }
 
 
     return (
@@ -93,8 +105,8 @@ export default function SingleMovie() {
 
                         ))}
                     </div>
-                    </div>
-                    <form className="card my-3" onSubmit={handleSubmit}>
+                </div>
+                <form className="card my-3" onSubmit={handleSubmit}>
                     <div className="mb-3 container">
                         <h3 className="bg-dark text-light text-uppercase">Insert your review</h3>
                         <label htmlFor="name" className="form-label">name</label>
@@ -106,7 +118,7 @@ export default function SingleMovie() {
                             aria-describedby="helpId"
                             placeholder="name"
                             value={formData.name}
-                            onChange={(e) => setFormData({...formData, name: e.target.value})}
+                            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
                         />
                         <label htmlFor="vote" className="form-label">Vote</label>
                         <input
@@ -119,7 +131,7 @@ export default function SingleMovie() {
                             aria-describedby="helpId"
                             placeholder="Insert your vote for the film"
                             value={formData.vote}
-                            onChange={(e) => setFormData({...formData, vote: e.target.value})}
+                            onChange={(e) => setFormData({ ...formData, vote: e.target.value })}
                         />
                         <div className="mb-3">
                             <label htmlFor="text" className="form-label">Text</label>
@@ -130,32 +142,32 @@ export default function SingleMovie() {
                                 rows="3"
                                 placeholder="Insert your review for the film"
                                 value={formData.text}
-                                onChange={(e) => setFormData({...formData, text: e.target.value})}
+                                onChange={(e) => setFormData({ ...formData, text: e.target.value })}
                             >
                             </textarea>
                         </div>
-                        
-                                <button
-                                    type="submit"
-                                    name=""
-                                    id=""
-                                    className="btn btn-primary"
-                                >
-                                    Send review
-                                </button>
-                            
-                            
+
+                        <button
+                            type="submit"
+                            name=""
+                            id=""
+                            className="btn btn-primary"
+                        >
+                            Send review
+                        </button>
+
+
 
                     </div>
-                    </form>
+                </form>
 
 
 
 
 
 
-                    <button className="btn btn-dark" onClick={() => navigate(-1)}>Back Home</button>
-                
+                <button className="btn btn-dark" onClick={() => navigate(-1)}>Back Home</button>
+
             </section>
         </>
     )
