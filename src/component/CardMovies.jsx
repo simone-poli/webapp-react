610d3977e215bccc8807a884f1cf8aafed49576c
simone-poli <polismn97@gmail.com>
2025-08-06 15:18:06 +0200

    add component cardmovies

--- src/component/CardMovies.jsx
+++ src/component/CardMovies.jsx
@@ -1,1 +1,22 @@
-export default
+import { Link } from "react-router-dom"
+
+
+export default function CardMovies({movie}) {
+
+    return (
+        <div className="card mt-3">
+            <div className="card-img-top">
+                <Link to={`/movies/${movie.id}`}>
+                    <img src={`${import.meta.env.VITE_SERVER_API_URL}${movie.image}`} alt="" />
+                </Link>
+            </div>
+
+            <div className="card-body text-decoration-none text-center">
+                <Link className="text-decoration-none" to={`/movies/${movie.id}`}>
+                    <h4 className="text-dark">{movie.title}</h4>
+                    <p className="text-dark">{movie.abstract}</p>
+                </Link>
+            </div>
+        </div>
+    )
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,4 @@
+import CardMovies from "../component/CardMovies"
 import { useGlobalContext } from "../context/GlobalContext"
 import { Link } from "react-router-dom"
 
@@ -15,20 +16,7 @@ export default function HomePage() {
                         movies?.map((movie) => {
                             return (
                                 <div className="col" key={movie.id}>
-                                    <div className="card mt-3">
-                                        <div className="card-img-top">
-                                            <Link to={`/movies/${movie.id}`}>
-                                                <img src={`${import.meta.env.VITE_SERVER_API_URL}${movie.image}`} alt="" />
-                                            </Link>
-                                        </div>
-
-                                        <div className="card-body text-decoration-none text-center">
-                                            <Link className="text-decoration-none" to={`/movies/${movie.id}`}>
-                                                <h4 className="text-dark">{movie.title}</h4>
-                                                <p className="text-dark">{movie.abstract}</p>
-                                            </Link>
-                                        </div>
-                                    </div>
+                                    <CardMovies movie={movie}></CardMovies>
                                 </div>
 
                             )
